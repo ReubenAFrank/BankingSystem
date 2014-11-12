9c12cfc25f068b87bee5082556d7a1d5e9ef80ac
ReubenAFrank <reuben.a.frank@gmail.com>
2014-11-12 12:13:41 -0600

    Create Bank.java
    The core of the program.

---
+++ Bank.java
@@ -0,0 +1,11 @@
+public class Bank{
+     public Bank(){
+     
+     }
+     /**
+      *Starts the operation of the banking system
+      */
+     public void start(){
+          System.out.println("Hello! Welcome to JBank");
+     }
+}
